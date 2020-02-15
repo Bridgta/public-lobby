894a072870df090ccb24ee6d4d14f4e9ae0e494f
Bridget Asser <bridgetasser@gmail.com>
2020-02-15 13:18:49 -0500

    setting up database and user routes

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("things are going well");
+//route import
+const userRoutes = require("./routes/user");
+
+//app
+const app = express();
+//db connection
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
 });
+
+//routes
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
